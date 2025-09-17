64b66767b0652ed9ddb6969523b217b1d2100151
jitenderjoshimindfire <jitenderj@mindfiresolutions.com>
2025-09-17 03:30:56 -0400

    feature: react router configuration

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AppRouter from "./routes/Router";
+
+const App = () => {
+    return(
+        <AppRouter/>
+    )
+}
+
+export default App;
---
+++ src/routes/Router.tsx
@@ -0,0 +1,46 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import React, { Suspense } from "react";
+import Home from "../pages/Home";
+import Header from "../pages/Header";
+import Footer from "../pages/Footer";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+
+const MainLayout = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-white  shadow-sm sticky top-0 z-50">
+        <Header />
+      </header>
+
+      <main className="flex-grow">
+        <Suspense fallback={<div>Loading.......</div>}>
+          <Outlet />
+        </Suspense>
+      </main>
+
+      <footer className="bg-grey-100  border-t border-gray-200 dark:border-gray-800">
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout/>,
+        children:[
+            {index:true, element: <Home/>},
+            {path: 'login', element: <Login/>},
+            {path: 'signup', element: <Signup/>}
+        ]
+    }
+])
+
+
+const AppRouter = () => {
+    return <RouterProvider router={router}/>
+}
+
+export default AppRouter;
